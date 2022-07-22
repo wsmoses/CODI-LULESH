566b2efd8fc3ffa78b73e1327a57464412ad9ffb
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2022-07-21 23:21:57 -0500

    Modify scripts to ensure reproducibility for SC22.

--- bench/ser-mpi-weak-scaling/script.py
+++ bench/ser-mpi-weak-scaling/script.py
@@ -9,7 +9,7 @@ def printfun(rank, blocklist,itercount):
         os.system("mpirun -n {} taskset -c 0-{} numactl -i all ".format(rank,rank-1)+ str(scriptdir) + "/../../lulesh-{}/lulesh2.0 -s {} -i {} > ".format(mode,s,itercount) + str(scriptdir) + "/ser-mpi_{}_{}_{}_{}.txt".format(mode,rank,itercount,s))
 
 itercount=100
-printfun(1, [96],itercount)
-printfun(8, [96],itercount)
-printfun(27, [96],itercount)
-printfun(64, [96],itercount)
+printfun(1, [48],itercount)
+printfun(8, [48],itercount)
+printfun(27, [48],itercount)
+printfun(64, [48],itercount)
